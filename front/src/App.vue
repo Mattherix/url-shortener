563c95fbb320d7183a29fa5a8b4d262818b8ca24
mattherix <mattherix@protonmail.com>
2021-12-06 13:50:05 +0100

    feat(front): create vue app with taildwind

---
+++ front/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid place-items-center">
+    <input placeholder="Collez votre lien pour le réduire" />
+    <p>fjkhdjkfdhkj</p>
+  </div>
+</template>
+
+<script>
+import './assets/tailwind.css'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
